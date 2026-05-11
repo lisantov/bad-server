bfd3839bcc9141cae5182442f70ef32eaff34aac
lisantov <lisantov@yandex.ru>
2026-05-11 15:14:33 +0700

    Fix: phone regex

--- backend/src/middlewares/validations.ts
+++ backend/src/middlewares/validations.ts
@@ -1,8 +1,7 @@
 import { Joi, celebrate } from 'celebrate'
 import { Types } from 'mongoose'
 
-// eslint-disable-next-line no-useless-escape
-export const phoneRegExp = /^(\+\d+)?(?:\s|-?|\(?\d+\)?)+$/
+export const phoneRegExp = /^(\+?\d[\d\s\-\(\)]*)$/
 
 export enum PaymentType {
     Card = 'card',
